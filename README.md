8f72232fc55091889fa98cc5deb8411a874509ea
Sushant Mutagekar <58685537+SushantMutagekar@users.noreply.github.com>
2021-01-29 09:55:33 +0530

    files changed

--- README.md
+++ README.md
@@ -12,8 +12,16 @@ Both system times may have some offset wrt each other. This needs to be correcte
 # Output
 Output file is a csv file containing one time stamp and all collumns of file 1 and corresponding rows of file 2.
 
-# Joining dataframes
-Full Outer join
-Inner Join
-Right Join
-Left Join
+# Date formats
+ A few possible cases
+ 2019-05-28 22:12:58 --->  %Y-%m-%d %H:%M:%S <br/>
+ 28/05/2019 22:12:58 --->  %d/%m/%Y %H:%M:%S <br/>
+ 28-Jan-21 01:48:39:417 --->  %d-%b-%y %H:%M:%S:%f <br/>
+ 28-January-21 01:48:39:417 --->  %d-%B-%y %H:%M:%S:%f <br/>
+
+# Joining dataframes/csv
+Let file1 (left) and file2 (right) be the two files to be joined on key/column Timestamp.<br/>
+Full Outer join - Joined file will have all rows of file1 and file2, NAN is filled in places where no record exists.<br/>
+Inner Join - Joined file will only the records corresponding to Timestamp which matches in both files.<br/>
+Right Join - Joined file will all the records of file2 and only the rows of file1 whose Timestamp matches with file2. If no match then NaN is filled.<br/>
+Left Join - Joined file will all the records of file1 and only the rows of file2 whose Timestamp matches with file1. If no match then NaN is filled.<br/>
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pandas as pd
 
+file1 = pd.read_csv("G:\\Python\\Python Basics\\Join-CSV\\file1.csv")
+file2 = pd.read_csv("G:\\Python\\Python Basics\\Join-CSV\\file2.csv")
 
-
-file1 = pd.read_csv("file1.csv")
-file2 = pd.read_csv("file2.csv")
+#%% see format in the files and add the format field in below commands
+Timeformat_file1 =   '%m/%d/%Y %H:%M:%S'
+Timeformat_file2 =  '%d-%b-%y %H:%M:%S:%f'
+#file1['new_RTC'] =  pd.to_datetime(file1['RTC'], format='%Y-%m-%d %H:%M:%S')
+file1['new_Time'] =  pd.to_datetime(file1['RTC'], format= Timeformat_file1)
+file2['new_Time'] =  pd.to_datetime(file2['Time'], format= Timeformat_file2)
+#%% Reduce precision and make both timestamps in same format
+file1['new_Time'] = file1['new_Time'].apply(lambda t: t.strftime('%d/%m/%Y %H:%M:%S'))
+file2['new_Time'] = file2['new_Time'].apply(lambda t: t.strftime('%d/%m/%Y %H:%M:%S'))
 
 #%%
-file1['RTC'] =  pd.to_datetime(file1['RTC'], format='%Y-%m-%d %H:%M:%S')
-file2['Time'] =  pd.to_datetime(file2['Time'], format='%d-%b-%y %H:%M:%S:%f')
-#%% Reduce precision and make both timestamps in same format
-
-file1['Time'] = file1['RTC'].astype('datetime64[s]')
-file2['Time'] = file2['Time'].astype('datetime64[s]')
-
+file1['new_Time'] = file1['new_Time'].astype('datetime64[s]')
+file2['new_Time'] = file2['new_Time'].astype('datetime64[s]')
 #%% Sync the times, add offset time to the file2
 # let file2 time be ahead by 2minutes 5sec = 125 sec
 import datetime
-file2['Time'] = file2['Time'] - datetime.timedelta(seconds=125) # seconds =0 if files are already in sync
+file2['new_Time'] = file2['new_Time'] - datetime.timedelta(seconds=0) # seconds =0 if files are already in sync
+
 #%% Left Join both the CSV files to retain all rows of file1 and only the corresponding matcing rows of file2
-df_left = pd.merge(file1, file2, on='Time', how='left')
+df_left = pd.merge(file1, file2, on='new_Time', how='left')
 df_left.to_csv("merged_file.csv")
